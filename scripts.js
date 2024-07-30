30d41972242c2204e4bd5986f4595868e6c74bb2
lblommesteyn <lblommes@uwo.ca>
2024-07-30 02:22:38 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Add any interactivity here if needed
+// For parallax effect
+document.addEventListener('scroll', function() {
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.pageYOffset;
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
